c7f47378b6689a81e264f594b22031408555d512
nachtz <nachtz@outlook.com>
2017-02-25 16:21:56 +0800

    500+

---
+++ 504. Base 7.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+	//"math/rand"
+	//"sort"
+	//"regexp"
+)
+
+/***************************************/
+//some common data struct and function using in leetcode.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func useLib() {
+	fmt.Println(strconv.Itoa(1))
+	fmt.Println(strings.Compare("1", "2"))
+	fmt.Println(math.Abs(1.0))
+	fmt.Println(unicode.IsDigit('1'))
+}
+
+func buildTree(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := new(TreeNode)
+	root.Val = nums[0]
+	ch := make(chan *TreeNode, len(nums))
+	ch <- root
+	nums = nums[1:]
+	for i := 0; i < len(nums); i++ {
+		tree := <-ch
+		if nums[i] == -1 {
+			tree.Left = nil
+		} else {
+			tree.Left = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Left
+		}
+		i++
+		if i == len(nums) || nums[i] == -1 {
+			tree.Right = nil
+		} else {
+			tree.Right = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Right
+		}
+	}
+	return root
+}
+
+func buildList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := &ListNode{
+		Val: nums[0],
+	}
+	tmp := root
+	for i := 1; i < len(nums); i++ {
+		tmp.Next = &ListNode{
+			Val: nums[i],
+		}
+		tmp = tmp.Next
+	}
+	return root
+
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+/**************************************************************/
+/*
+type IntSlice []int
+
+func (s IntSlice)Less(i,j int)bool{
+	return s[i]<s[j]
+}
+func (s IntSlice)Swap(i,j int){
+	s[i],s[j] = s[j],s[i]
+}
+func (s IntSlice)Len()int{
+	return len(s)
+}
+*/
+/**************************************************************/
+func convertToBase7(num int) string {
+	if num == 0 {
+		return "0"
+	}
+	ret := ""
+	flag := false
+	if num < 0 {
+		flag = true
+		num = -num
+	}
+	for num > 0 {
+		a := num % 7
+		ret = strconv.Itoa(a) + ret
+		num = num / 7
+	}
+	if flag {
+		ret = "-" + ret
+	}
+	return ret
+}
+
+func main() {
+	fmt.Println(convertToBase7(-1))
+}
