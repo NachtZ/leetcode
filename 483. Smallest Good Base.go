55cbccc70c066e3fc3a60dce349bfa50f0b00977
nachtz <nachtz@outlook.com>
2017-03-11 22:18:08 +0800

    444

---
+++ 483. Smallest Good Base.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+	//"math/rand"
+	//"sort"
+	//"regexp"
+)
+
+/***************************************/
+//some common data struct and function using in leetcode.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func useLib() {
+	fmt.Println(strconv.Itoa(1))
+	fmt.Println(strings.Compare("1", "2"))
+	fmt.Println(math.Abs(1.0))
+	fmt.Println(unicode.IsDigit('1'))
+}
+
+func buildTree(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := new(TreeNode)
+	root.Val = nums[0]
+	ch := make(chan *TreeNode, len(nums))
+	ch <- root
+	nums = nums[1:]
+	for i := 0; i < len(nums); i++ {
+		tree := <-ch
+		if nums[i] == -1 {
+			tree.Left = nil
+		} else {
+			tree.Left = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Left
+		}
+		i++
+		if i == len(nums) || nums[i] == -1 {
+			tree.Right = nil
+		} else {
+			tree.Right = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Right
+		}
+	}
+	return root
+}
+
+func buildList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := &ListNode{
+		Val: nums[0],
+	}
+	tmp := root
+	for i := 1; i < len(nums); i++ {
+		tmp.Next = &ListNode{
+			Val: nums[i],
+		}
+		tmp = tmp.Next
+	}
+	return root
+
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+/**************************************************************/
+/*
+type IntSlice []int
+
+func (s IntSlice)Less(i,j int)bool{
+	return s[i]<s[j]
+}
+func (s IntSlice)Swap(i,j int){
+	s[i],s[j] = s[j],s[i]
+}
+func (s IntSlice)Len()int{
+	return len(s)
+}
+
+type StrSlice []string
+
+func (s StrSlice) Less(i, j int) bool {
+	if len(s[i]) != len(s[j]) {
+		return len(s[i]) > len(s[j])
+	}
+	return s[i] < s[j]
+}
+func (s StrSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s StrSlice) Len() int {
+	return len(s)
+}
+
+*/
+/**************************************************************/
+func smallestGoodBase(n string) string {
+	t, _ := strconv.ParseUint(n, 10, 64)
+	var length, l, r, mid, res uint64
+	found := uint64(1)
+	for length = 1; (1 << length) <= t; length++ {
+	}
+	l, r, mid = 2, length, 2
+	for ; length > 0; length-- {
+		l, r = 2, uint64(math.Pow(float64(t), 1.0/float64(length))+1)
+		for l <= r {
+			mid = l + (r-l)/2
+
+			res = 0
+			for tmp, i := uint64(1), 0; uint64(i) <= length; i++ {
+				res += tmp
+				tmp = tmp * mid
+			}
+			//fmt.Println(l, r, mid, t, res)
+			if res == t {
+				//return strconv.FormatUint(mid, 10)
+				found = mid
+			}
+
+			if res > t {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+		if found > 1 {
+			return strconv.FormatUint(found, 10)
+		}
+	}
+	return strconv.FormatUint(t-1, 10)
+}
+func main() {
+
+	fmt.Println(smallestGoodBase("727004545306745403"))
+}
