104151bec6fd24d53b45c05f04e1ae5751dc5153
nachtz <nachtz@outlook.com>
2017-06-05 23:04:47 +0800

    557

---
+++ 556. Next Greater Element III.go
@@ -0,0 +1,291 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+	//"math/rand"
+	//"sort"
+
+	"sort"
+)
+
+/***************************************/
+//some common data struct and function using in leetcode.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func useLib() {
+	fmt.Println(strconv.Itoa(1))
+	fmt.Println(strings.Compare("1", "2"))
+	fmt.Println(math.Abs(1.0))
+	fmt.Println(unicode.IsDigit('1'))
+	fmt.Println(sort.IsSorted(nil))
+}
+
+func buildTree(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := new(TreeNode)
+	root.Val = nums[0]
+	ch := make(chan *TreeNode, len(nums))
+	ch <- root
+	nums = nums[1:]
+	for i := 0; i < len(nums); i++ {
+		tree := <-ch
+		if nums[i] == -1 {
+			tree.Left = nil
+		} else {
+			tree.Left = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Left
+		}
+		i++
+		if i == len(nums) || nums[i] == -1 {
+			tree.Right = nil
+		} else {
+			tree.Right = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Right
+		}
+	}
+	return root
+}
+
+func printTreeNode(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	left, right := "nil", "nil"
+	if root.Left != nil {
+		left = strconv.Itoa(root.Left.Val)
+	}
+	if root.Right != nil {
+		right = strconv.Itoa(root.Right.Val)
+	}
+	fmt.Println(root.Val, ":", left, right)
+	printTreeNode(root.Left)
+	printTreeNode(root.Right)
+}
+
+func buildList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := &ListNode{
+		Val: nums[0],
+	}
+	tmp := root
+	for i := 1; i < len(nums); i++ {
+		tmp.Next = &ListNode{
+			Val: nums[i],
+		}
+		tmp = tmp.Next
+	}
+	return root
+
+}
+func printList(root *ListNode) {
+	for root != nil {
+		fmt.Print(root.Val, "->")
+		root = root.Next
+	}
+	fmt.Println("nil")
+}
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+type trie struct {
+	ch    byte
+	child [26]*trie
+	flag  bool // true means here located a word.
+}
+
+func (this *trie) insertWords(str string) {
+	ptr := this
+	for idx, s := range str {
+		if ptr.child[s-'a'] == nil {
+			ptr.child[s-'a'] = new(trie)
+			ptr.child[s-'a'].ch = str[idx]
+		}
+		if idx == len(str)-1 {
+			ptr.child[s-'a'].flag = true
+		}
+		ptr = ptr.child[s-'a']
+	}
+}
+
+func (this *trie) searchWords(str string) *trie {
+	ptr := this
+	for idx, s := range str {
+		if ptr.child[s-'a'] == nil {
+			return nil
+		}
+		if idx == len(str)-1 {
+			if ptr.child[s-'a'].flag == true {
+				return ptr.child[s-'a']
+			}
+			return nil
+		}
+		ptr = ptr.child[s-'a']
+
+	}
+	return nil
+}
+
+func buildTrie(words []string) *trie {
+	root := &trie{}
+	for _, w := range words {
+		root.insertWords(w)
+	}
+	return root
+}
+
+/**************************************************************/
+
+type IntSlice []int
+
+func (s IntSlice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s IntSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s IntSlice) Len() int {
+	return len(s)
+}
+
+type StrSlice []string
+
+func (s StrSlice) Less(i, j int) bool {
+	if len(s[i]) != len(s[j]) {
+		return len(s[i]) > len(s[j])
+	}
+	return s[i] < s[j]
+}
+func (s StrSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s StrSlice) Len() int {
+	return len(s)
+}
+
+/**************************************************************/
+
+/**
+ * Definition for an interval.
+ * type Interval struct {
+ *	   Start int
+ *	   End   int
+ * }
+ */
+
+/**************************************************************/
+func nextGreaterElement(n int) int {
+	if n < 10 {
+		return -1
+	}
+	nums := []int{}
+	for n > 0 {
+		nums = append(nums, n%10)
+		n /= 10
+	}
+	found := false
+	for i := 1; !found && i < len(nums); i++ {
+		if nums[i-1] > nums[i] {
+			found = true
+		}
+	}
+	if found == false {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+	minV := math.MaxInt32 + 1
+	rightMark := -1
+	for left < right {
+		i := left + 1
+		for i = left + 1; i <= right; i++ {
+			if nums[left] > nums[i] {
+				nums[left], nums[i] = nums[i], nums[left]
+				t := 0
+				for i := len(nums) - 1; i >= 0; i-- {
+					t = t*10 + nums[i]
+				}
+				if t < minV {
+					minV = t
+					rightMark = i
+				}
+				nums[left], nums[i] = nums[i], nums[left]
+				right = i
+				left++
+				break
+			}
+		}
+		if i > right {
+			left++
+		}
+	}
+	if rightMark == -1 {
+		return -1
+	}
+	for i := 0; minV > 0; i++ {
+		nums[i] = minV % 10
+		minV = minV / 10
+	}
+	tnum := nums[:rightMark]
+	sort.Ints(tnum)
+	t := 0
+	for i := len(nums) - 1; i >= rightMark; i-- {
+		t = t*10 + nums[i]
+	}
+	for i := 0; i < rightMark; i++ {
+		t = t*10 + nums[i]
+	}
+	if t > math.MaxInt32 {
+		return -1
+	}
+	return t
+}
+
+/**/
+func main() {
+	fmt.Println(nextGreaterElement(1999999999))
+}
