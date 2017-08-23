44b48149d3d330ff2326963603c7350e4f4023b4
nachtz <nachtz@outlook.com>
2017-08-23 21:46:41 +0800

    630

---
+++ 630. Course Schedule III.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+	//"math/rand"
+	//"sort"
+	"regexp"
+	"sort"
+)
+
+/***************************************/
+//some common data struct and function using in leetcode.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+func useLib() {
+	fmt.Println(strconv.Itoa(1))
+	fmt.Println(strings.Compare("1", "2"))
+	fmt.Println(math.Abs(1.0))
+	fmt.Println(unicode.IsDigit('1'))
+	fmt.Println(sort.IsSorted(nil))
+	fmt.Println(regexp.QuoteMeta(""))
+}
+
+func buildTree(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := new(TreeNode)
+	root.Val = nums[0]
+	ch := make(chan *TreeNode, len(nums))
+	ch <- root
+	nums = nums[1:]
+	for i := 0; i < len(nums); i++ {
+		tree := <-ch
+		if nums[i] == -1 {
+			tree.Left = nil
+		} else {
+			tree.Left = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Left
+		}
+		i++
+		if i == len(nums) || nums[i] == -1 {
+			tree.Right = nil
+		} else {
+			tree.Right = &TreeNode{
+				Val: nums[i],
+			}
+			ch <- tree.Right
+		}
+	}
+	return root
+}
+
+func printTreeNode(root *TreeNode) {
+	if root == nil {
+		fmt.Println("nil")
+		return
+	}
+	left, right := "nil", "nil"
+	if root.Left != nil {
+		left = strconv.Itoa(root.Left.Val)
+	}
+	if root.Right != nil {
+		right = strconv.Itoa(root.Right.Val)
+	}
+	fmt.Println(root.Val, ":", left, right)
+	printTreeNode(root.Left)
+	printTreeNode(root.Right)
+}
+
+func buildList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := &ListNode{
+		Val: nums[0],
+	}
+	tmp := root
+	for i := 1; i < len(nums); i++ {
+		tmp.Next = &ListNode{
+			Val: nums[i],
+		}
+		tmp = tmp.Next
+	}
+	return root
+
+}
+func printList(root *ListNode) {
+	for root != nil {
+		fmt.Print(root.Val, "->")
+		root = root.Next
+	}
+	fmt.Println("nil")
+}
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+type trie struct {
+	ch    byte
+	child [26]*trie
+	flag  bool // true means here located a word.
+}
+
+func (this *trie) insertWords(str string) {
+	ptr := this
+	for idx, s := range str {
+		if ptr.child[s-'a'] == nil {
+			ptr.child[s-'a'] = new(trie)
+			ptr.child[s-'a'].ch = str[idx]
+		}
+		if idx == len(str)-1 {
+			ptr.child[s-'a'].flag = true
+		}
+		ptr = ptr.child[s-'a']
+	}
+}
+
+func (this *trie) searchWords(str string) *trie {
+	ptr := this
+	for idx, s := range str {
+		if ptr.child[s-'a'] == nil {
+			return nil
+		}
+		if idx == len(str)-1 {
+			if ptr.child[s-'a'].flag == true {
+				return ptr.child[s-'a']
+			}
+			return nil
+		}
+		ptr = ptr.child[s-'a']
+
+	}
+	return nil
+}
+
+func buildTrie(words []string) *trie {
+	root := &trie{}
+	for _, w := range words {
+		root.insertWords(w)
+	}
+	return root
+}
+
+/**************************************************************/
+type StrSlice []string
+
+func (s StrSlice) Less(i, j int) bool {
+	if len(s[i]) != len(s[j]) {
+		return len(s[i]) < len(s[j])
+	}
+	return s[i] < s[j]
+}
+func (s StrSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s StrSlice) Len() int {
+	return len(s)
+}
+
+/***************************************************/
+type CourseSlice [][]int
+
+func (s CourseSlice) Less(i, j int) bool {
+	if s[i][1] != s[j][1] {
+		return s[i][1] < s[j][1]
+	}
+	return s[i][0] < s[j][0]
+}
+
+func (s CourseSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s CourseSlice) Len() int {
+	return len(s)
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func scheduleCourse(courses [][]int) int {
+	if len(courses) <= 1 {
+		return len(courses)
+	}
+	sort.Sort(CourseSlice(courses))
+	h := new(IntHeap)
+	heap.Init(h)
+	lastDay := 0
+	for i := 0; i < len(courses); i++ {
+		heap.Push(h, courses[i][0])
+		lastDay += courses[i][0]
+		if lastDay > courses[i][1] {
+			t := heap.Pop(h).(int)
+			lastDay -= t
+		}
+
+	}
+	return len(*h)
+}
+func main() {
+	fmt.Println(scheduleCourse([][]int{[]int{100, 200},
+		[]int{200, 1300},
+		[]int{1000, 1250},
+		[]int{2000, 3200},
+	}))
+}
